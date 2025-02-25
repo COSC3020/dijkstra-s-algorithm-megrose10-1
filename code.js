cdba01681c0f4be6cd0623f5b37a18eab8aef332
megrose10 <157438545+megrose10@users.noreply.github.com>
2025-02-24 20:31:55 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,59 @@
 function dijkstra(graph, sourceNode) {
+//for my graph I will be using adj list as my representation
+//initialize the dist to each vertex to infinity, source to 0
+
+    let dist = {};
+    let visited = {};
+
+    //variable to follow if we still have vertices to visit
+    let needToVisit = true;
+
+    for(let vertex in graph) {
+        dist[vertex] = Infinity;
+        visited[vertex] = false;
+    }
+
+    dist[sourceNode] = 0;
+
+    //While there are unmarked vertices left in the graph
+    while(needToVisit) {
+        //select the unmarked vertex v with the lowest distance
+        let minDistVert = null;
+        let minDist = Infinity;
+        for(v in graph) {
+            if(needToVisit) {
+                if(dist[v] < minDist) {
+                    minDist = dist[v];
+                    minDistVert = v;
+                }
+            }
+        }
+
+        visited[minDistVert] = true;
+
+        for(let w in graph[minDistVert]) {
+
+            let weight = graph[minDistVert][w];
+            let updatedDist = dist[minDistVert] + weight;
+
+            if(updatedDist < dist[w]) {
+                dist[w] = updatedDist;
+            }
+
+        }
+
+        for(let visitedVert in visted) {
+            if(!visited) {
+                break;
+            }
+            needToVisit = false;
+        }
+        
+    }
+
     return [];
+
 }
+
+
+//https://www.geeksforgeeks.org/objects-in-javascript/
